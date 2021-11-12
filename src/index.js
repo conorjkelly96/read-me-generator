9ccd96bb03d48d4ca6b2a8a2abd7907f662d7118
Conor Kelly <conorjkelly96@gmail.com>
2021-11-12 22:04:31 +0000

    pseudocode to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("hi");
+// install inquirer
+const inquirer = require("inquirer");
+
+// questions that will be prompted to the user
+const mainQuestions = [];
+
+// initialise user interaction
+const init = async () => {
+  // prompt questions
+  // generate read me
+  // write readme generated
+};
+
+init();
