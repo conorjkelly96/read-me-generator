52e3477f7edb4141229060ec373e39aedb9696e2
Conor Kelly <conorjkelly96@gmail.com>
2021-11-13 16:57:19 +0000

    hard code tests added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,47 @@
 // install inquirer
 const inquirer = require("inquirer");
 const utils = require("./utils");
-const fileGen = require("./generate-file");
+const fileGen = require("./file-gen");
 
 // questions that will be prompted to the user
-const mainQuestions = [];
+const mainQuestions = [
+  {
+    type: "input",
+    name: "firstName",
+    message: "What is your first name?",
+  },
+];
+
+// generating Read Me document
+const generateReadme = (answers) => {
+  return `${generateTitle(answers)}
+
+  ${generateTableOfContents(answers)}
+  
+  ${generateDescription(answers)}
+  
+  ${generateInstallation(answers)}
+  
+  ${generateUsage(answers)}
+  
+  ${generateTests(answers)}
+  
+  ${generateContributing(answers)}
+  
+  ${generateLicense(answers)}
+  `;
+};
 
 // initialize user interaction
 const init = async () => {
   // prompt questions
   const answers = await inquirer.prompt(mainQuestions);
+
   // generate read me
+  const readMe = generateReadme(answers);
 
   // write readme generated
+  fileGen.writeToFile("README.md", readMe);
 };
 
 init();
--- src/utils.js
+++ src/utils.js
@@ -3,31 +3,67 @@
 // generate title
 const generateTitle = (answers) => {
   // place pseudocode here
+  return `# TITLE ![MIT](https://img.shields.io/static/v1?label=MIT&message=License&color=green)`;
 };
 
 // generate table of contents
 const generateToC = (answers) => {
   // place pseudocode here
+  return `## Table of Contents
+  
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Tests](#tests)
+  - [Contributing](#contributing)
+  - [License](#license)`;
 };
 
 // generate description
 const generateDescription = (answers) => {
   // place pseudocode here
+  return `## Description
+  
+  ADD TEXT HERE`;
 };
 
 // generate the installation
 const generateInstallation = (answers) => {
   // place pseudocode here
+  return `## Installation
+  
+  Run the following script to install the packages required for the application:
+  
+  \`\`\`
+  ADD TEXT HERE
+  \`\`\``;
 };
 
 // generate the Usage
 const generateUsage = (answers) => {
   // place pseudocode here
+  return `## Usage
+  
+  To use the application run the following script:
+  
+  \`\`\`
+  ADD TEXT HERE
+  \`\`\``;
+};
+
+// generate the Contributing section
+const generateContributing = (answers) => {
+  return `## Contributing
+  
+  ADD TEXT HERE`;
 };
 
 // generate the License
 const generateLicense = (answers) => {
   // place pseudocode here
+  return `## License
+  
+  ADD TEXT HERE`;
 };
 
 // exporting modules for external use
@@ -35,6 +71,7 @@ module.exports = {
   generateTitle,
   generateToC,
   generateDescription,
+  generateContributing,
   generateLicense,
   generateInstallation,
   generateUsage,
